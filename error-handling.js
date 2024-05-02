551209fdd81ea807f34f07fb43a103bb06cfc22b
Imani Roberts <imaniroberts.dev@gmail.com>
2024-05-01 20:51:11 -0400

    finished 1 and 2

--- error-handling.js
+++ error-handling.js
@@ -1,20 +1,37 @@
-// 1.
-function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+// // 1.
+// function sum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+//   }
+//   return sum;
+// }
 
-let res = sum(null);
-console.log(res);
+// let res = sum(null);
+// // console.log(res);
+
+// //tests
+// try {
+//   sum(null);
+// } catch (error) {
+//   throw`Sorry, but null is not an array`;
+// }
 
 // 2.
+
+function sayName(string) {
+  if (typeof string !== "string") {
+    throw TypeError;
+  }
+  return string;
+}
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here 
+try {
+  sayName("Alex");
+  sayName(1);
+} catch (error) {
+  console.log(`Invalid name! Must be a string!`);
+}
 
 // 3.
 function greet(greeting) {
@@ -24,4 +41,3 @@ function greet(greeting) {
 
   console.log(greeting);
 }
-
